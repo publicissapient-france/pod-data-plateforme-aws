a50f31ea4274a7695c27f7c6dd9472d416e9449e
aphelip <alban.phelip@gmail.com>
2021-02-15 19:34:26 +0100

    Incoming to Raw works

--- lambda/pod-data-platform-move-data-to-raw/lambda_function.py
+++ lambda/pod-data-platform-move-data-to-raw/lambda_function.py
@@ -7,27 +7,30 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 def lambda_handler(event, context):
+    logger.info(f'Event: {event}')
+
     s3 = boto3.resource('s3')
 
+    event_key = event['event_key']
+    filename = os.path.basename(event_key)
+    source_name = event_key.split("/")[0]
+    table_name = event_key.split("/")[1]
+
+    incoming_bucket = os.environ['INCOMINGBUCKETNAME']
+    raw_bucket = os.environ['RAWBUCKETNAME']
+
+    copy_source = {
+        'Bucket': incoming_bucket,
+        'Key': event_key
+    }
+
     try:
-        event_key = event['key']
-        filename = os.path.basename(event_key)
-        source_name = event_key.split("/")[0]
-        table_name = event_key.split("/")[1]
-
-        incoming_bucket = os.environ['INCOMINGBUCKETNAME']
-        raw_bucket = os.environ['RAWBUCKETNAME']
-
-        copy_source = {
-            'Bucket': incoming_bucket,
-            'Key': event_key
-        }
-        s3.meta.client.copy(copy_source, raw_bucket, filename)
+        s3.meta.client.copy(copy_source, raw_bucket, event_key)
     except Exception as e:
-        logger.error(f"Not able to copy {event_key} incoming file")
+        logger.error(f"Not able to copy {event_key} incoming file. Cause: {e}")
         s3.meta.client.copy({'Bucket': incoming_bucket, 'Key': event_key}, incoming_bucket, f'error/{event_key}')
     finally:
-        logger.error(f"Deleting {event_key}")
+        logger.info(f"Deleting {event_key}")
         s3.Object(incoming_bucket, event_key).delete()
 
     return json.dumps({
